0fe29c5156d1847ab4fcb6c4cc62300a2b4bc09a
Nick Tomlin <nick.tomlin@gmail.com>
2016-11-20 22:00:08 -0600

    chore: Make nawk a class and move one time parsing into constructor

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ let process = require('./src/process')
 let parseArgs = require('./src/parse-args')
 let reifyCommands = require('./src/reify-commands')
 
-function nawk (input, argv) {
-  let [expression, ...args] = argv || []
-  let [tree] = parseArgs(args)
-  let commands = reifyCommands(tree)
-  let processedLine = process(input, expression)
+class Nawk {
+  constructor (argv) {
+    let [expression, ...args] = argv || []
+    let [tree] = parseArgs(args)
 
-  return commands.reduce((accum, command) => {
-    if (command.conditions.every((f) => {
-      return f.fn(processedLine)
-    })) {
-      return command.fn(processedLine)
-    } else {
-      return input
-    }
-  }, processedLine)
+    this.expression = expression
+    this.commands = reifyCommands(tree)
+  }
+
+  processLine (line) {
+    let processedLine = process(line, this.expression)
+
+    return this.commands.reduce((accum, command) => {
+      if (command.conditions.every((f) => {
+        return f.fn(processedLine)
+      })) {
+        return command.fn(processedLine)
+      } else {
+        return line
+      }
+    }, processedLine)
+  }
 }
 
-module.exports = nawk
+module.exports = Nawk
--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,28 @@
 'use strict'
 
 let test = require('ava').test
-let nawk = require('../index')
+let Nawk = require('../index')
 
 test('processes input', t => {
   let args = ['$0', '--append', ';', '--unless', '/bad/']
+  let nawk = new Nawk(args)
 
-  t.is(nawk('hello', args), 'hello;')
-  t.is(nawk('bad hello', args), 'bad hello')
+  t.is(nawk.processLine('hello'), 'hello;')
+  t.is(nawk.processLine('bad hello'), 'bad hello')
 })
 
 test('handles a an expression and conditions', t => {
   let args = ['The whole enchillada: $0 and time $1', '--append', ';', '--unless', '/bad/']
   let input = 'tab0 tab1 tab2'
+  let nawk = new Nawk(args)
 
-  t.is(nawk(input, args), 'The whole enchillada: tab0 tab1 tab2 and time tab0;')
+  t.is(nawk.processLine(input), 'The whole enchillada: tab0 tab1 tab2 and time tab0;')
 })
 
 test('handles commands without conditions', t => {
   let args = ['The whole enchillada: $0 and time $1', '--append', ';']
   let input = 'tab0 tab1 tab2'
+  let nawk = new Nawk(args)
 
-  t.is(nawk(input, args), 'The whole enchillada: tab0 tab1 tab2 and time tab0;')
+  t.is(nawk.processLine(input, args), 'The whole enchillada: tab0 tab1 tab2 and time tab0;')
 })
